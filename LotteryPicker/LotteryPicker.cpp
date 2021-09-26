5e230ec4b9c3032b17b6c5eda94d708d3ee56d66
Nathan Dilla <nathcdilla@protonmail.com>
2021-09-25 19:51:56 -0700

    complete rewrite, closely matches prompt

--- LotteryPicker/LotteryPicker.cpp
+++ LotteryPicker/LotteryPicker.cpp
@@ -1,85 +1,83 @@
-// LotteryPicker.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//  main.cpp
+//  LotteryPicker
+//
+//  Created by Nathan Dilla on 9/25/21.
 //
 
+// insert code here...
 /*
-Power Ball selects 5 numbers in the range 1-69, inclusive, and one number in the range 1-26, inclusive, as the power ball.
-The 5 numbers cannot have any repeat numbers.  Once the 5 numbers are selected, they are displayed in ascending order.
+ Power Ball selects 5 numbers in the range 1-69, inclusive, and one number in the range 1-26, inclusive, as the power ball. The 5 numbers cannot have any repeat numbers.  Once the 5 numbers are selected, they are displayed in ascending order.
 
-Create a class that that generates a lottery number picker.
-Your class will have a method that must store all 6 numbers in an array and return an array. 
-The method cannot be void. Your program should ask the user how many lottery numbers 
-they would like to see and offer a chance to generate more.
-To display the numbers, output the 5 main numbers and the power ball number as shown in the "Lottery Picker Sample Run."
-
--Nathan Dilla 9/19/21
-*/
+ Create a class that that generates a lottery number picker. Your class will have a method that must store all 6 numbers in an array and return an array.  The method cannot be void. Your program should ask the user how many lottery numbers they would like to see and offer a chance to generate more. To display the numbers, output the 5 main numbers and the power ball number as shown in the "Lottery Picker Sample Run."
+ */
 
 #include <iostream>
-#include <ctime>
-#include<cstdlib>
+#include <stdlib.h>
+#include <time.h>
 #include <algorithm>
 using namespace std;
 
-void randomize(int* arr, int size, int index, int limit) // randomizing function
+int getRandomNumber(int * array, int limit, int size)
 {
-    while (true) //infinite loop search for unique, random number
+    int randomNumber = rand() % limit;
+    for (int i = 0; i < size; i++)
     {
-        arr[index] = rand() % limit;
-        for (int j = 0; j < size; j++)
+        if(randomNumber == array[i])
         {
-            if (arr[index] == arr[j]) //if similar to another number, loop back
-            {
-                continue;
-            }
+            getRandomNumber(array, limit, size);
         }
-        break; //assume no similatiy, break loop
     }
-    
+    return randomNumber;
 }
 
-int* lottery(int* arr, int size)
+int * getNumbers()
 {
-    for (int i = 0; i < size; i++) //loop through each cel in array
+    static int arr[6];
+    for (int i = 0; i < 5; i++)
     {
-        if (i == size - 1) //if final index (power ball) call randomize limit 1-26
-        {
-            randomize(arr, size, i, 27);
-        }
-        else
-        {
-            randomize(arr, size, i, 70); //call randomize for other 5 numbers
-        }
-        
+        arr[i]=getRandomNumber(arr, 70, 5);
     }
+    sort(arr, arr+5);
+    arr[5] = getRandomNumber(arr, 27, 5);
     return arr;
 }
 
-void printArray(const int* list, int size) //prints array
-{
-    for (int i = 0; i < size; i++)
-        std::cout << list[i] << " ";
-}
-
-int main()
-{
-    srand(time(0)); //generate random seed from unix time
-    string response;
-    while (true) //infinite loop asking user
+int main(int argc, const char * argv[]) {
+    int numberOfArrays;
+    
+    srand(time(0));
+    while(true)
     {
-        std::cout << "\n Would you like to generate a lottery? (Y/N)" << endl;
-        std::cin >> response; // store answer from prompt
-        if (response == "Y") 
+        cout << "How many winning numbers would you like to see?  ";
+        cin >> numberOfArrays;
+        
+        for (int i = 0; i < numberOfArrays; i++)
         {
-            int list[6]; //declare new array
-            int* ptr = lottery(list, 6); //create poiner from returned randomized lottery numbers
-            sort(ptr, ptr + 6); //sort to ascending order
-            printArray(ptr, 6); //print array
+            int * arr = getNumbers();
+            cout << "Your numbers are: ";
+            for (int i = 0; i < 5; i++)
+            {
+                cout << arr[i] << " -";
+            }
+            
+            cout<<"Powerball: "<<arr[5];
+            cout << endl;
+        }
+        
+        cout<<"would you like to see more numbers? (y/n)";
+        string input;
+        cin>>input;
+        if(input == "y")
+        {
+            continue;
         }
         else
         {
-            cout << "Goodbye";
-            break; //break loop from answering no.
+            break;
         }
     }
+    
+    cout<<"Goodbye! :-)";
     return 0;
 }
